c9cf50fd538824a35169e84acbab708962f2d54c
DenisKhekalo <66767388+DenisKhekalo@users.noreply.github.com>
2020-10-12 22:32:06 +0600

    updated

---
+++ Practice_2/js/script.js
@@ -0,0 +1,8 @@
+let totalCash = (prompt("Сколько бабок есть Кевин?"))
+let watchesCount = (prompt("Сколько на этот раз купишь часов?"))
+
+let earringsCount = (prompt("Сколько на этот раз купишь драгоченных камней?"))
+let watchesSum = (prompt("Сколько стоят одни часы, Бро?"))
+let earringsSum = (prompt("Сколько стоит один драгоценный камень,Бро?"))
+
+Number(totalCash) >= Number(watchesCount)*Number(watchesSum)+Number(earringsCount)*Number(earringsSum)
